d600013d451a3d20a986e57d24ec354412884654
v.kuz <v.kuz@skbkontur.ru>
2024-08-04 03:23:12 +0500

    fix: correct pm2 serve env variables to run locally

--- Magic.Frontend/ecosystem.config.js
+++ Magic.Frontend/ecosystem.config.js
@@ -4,6 +4,7 @@ module.exports = {
         PM2_SERVE_PATH: 'build',
         PM2_SERVE_PORT: 3000,
         NODE_ENV: "development",
-        MAGIC_API_PROXY: "https://localhost:7021/api/v1/"
+        MAGIC_API_PROXY: "api/v1/",
+        MAGIC_PROXY: "https://localhost:7021/"
     }
 }
