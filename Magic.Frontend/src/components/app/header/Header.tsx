da8103f079eaa71e17a23dac0c06c73bec9d61ac
Albert <a.teregulov@betu2.com>
2024-08-11 18:43:21 +0300

    chage header

--- Magic.Frontend/src/components/app/header/Header.tsx
+++ Magic.Frontend/src/components/app/header/Header.tsx
@@ -1,62 +1,67 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import {useNavigate} from "react-router-dom";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Button from "@mui/material/Button";
+import { Link, useNavigate } from "react-router-dom";
 import paths from "../../../consts/paths";
-import {useSelector} from "react-redux";
-import {RootState} from "../../../redux/redux";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../redux/redux";
 import Account from "./account/Account";
-import {PaletteMode} from "@mui/material";
+import { PaletteMode } from "@mui/material";
 import ToggleColorMode from "./ToggleColorMode";
-
-const logoStyle = {
-    width: '140px',
-    height: 'auto',
-    cursor: 'pointer',
-};
+import { Logo } from "../../../assets/vector/Logo";
+import className from "./header.styles";
 
 interface HeaderProps {
-    mode: PaletteMode;
-    toggleColorMode: () => void;
+  mode: PaletteMode;
+  toggleColorMode: () => void;
 }
 
-export default function Header({mode, toggleColorMode}: HeaderProps) {
-    const navigate = useNavigate();
-    const isLoggedIn = useSelector((state: RootState) => state.auth.token !== null);
+export default function Header({ mode, toggleColorMode }: HeaderProps) {
+  const navigate = useNavigate();
+  const isLoggedIn = useSelector(
+    (state: RootState) => state.auth.token !== null
+  );
 
-    return (
-        <AppBar position="sticky" color="inherit">
-            <Toolbar>
-                <Box component="div" sx={{flexGrow: 1}}>
-                    <Typography variant="h6" component="div" style={logoStyle} onClick={onHomeClick}>
-                        Magic
-                    </Typography>
-                </Box>
-                <ToggleColorMode mode={mode} toggleColorMode={toggleColorMode}/>
-                {isLoggedIn
-                    ? <Account/>
-                    : <>
-                        <Button onClick={onRegisterClick} color="inherit">Register</Button>
-                        <Button onClick={onLoginClick} color="inherit">Login</Button>
-                    </>
-                }
+  
+  return (
+    <AppBar position="sticky" color="inherit">
+      <Toolbar style={className.header}>
+        <Link to={paths.home} style={className.logo}>
+          <Logo />
+          <span>
+            Magic
+          </span>
+        </Link>
+        <Box style={className.rightController}>
+          <ToggleColorMode mode={mode} toggleColorMode={toggleColorMode} />
+          {isLoggedIn ? (
+            <Account />
+          ) : (
+            <>
+              <Button onClick={onRegisterClick} color="inherit">
+                Register
+              </Button>
+              <Button onClick={onLoginClick} color="inherit">
+                Login
+              </Button>
+            </>
+          )}
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
 
-            </Toolbar>
-        </AppBar>
-    );
+  function onHomeClick() {
+    navigate(paths.home);
+  }
 
-    function onHomeClick() {
-        navigate(paths.home)
-    }
+  function onLoginClick() {
+    navigate(paths.login);
+  }
 
-    function onLoginClick() {
-        navigate(paths.login)
-    }
-
-    function onRegisterClick() {
-        navigate(paths.register)
-    }
+  function onRegisterClick() {
+    navigate(paths.register);
+  }
 }
---
+++ Magic.Frontend/src/components/app/header/header.styles.ts
@@ -0,0 +1,24 @@
+import { borderBottom, fontSize } from "@mui/system"
+
+const className = {
+  header: {
+    display: 'flex',
+    height: '80px',
+    borderBottom: 'solid 1px #5C5C5D',
+    justifyContent: 'space-between'
+  },
+  logo: {
+    display: 'flex',
+    color: 'inherit',
+    gap: '.5em',
+    fontSize: '2em',
+    alignItems: 'center',
+    textDecoration: 'none'
+  },
+  rightController: {
+    display: 'flex',
+    alignItems: 'center',
+  }
+}
+
+export default className
